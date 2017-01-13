77aeedbb0e42b0ce3a33b0f49edcaf4d7e7c5b0c
Rehum02 <kokoramiche02@yahoo.com>
2017-01-13 15:49:26 +0000

    Create padi.php

---
+++ padi.php
@@ -0,0 +1,129 @@
+<html> 
+<head >  
+<meta charset="utf-8">
+<style>
+p
+{
+	color :blue ;
+}
+</style>
+<title> KOKORA the son of God
+</title>
+</head>  
+<body  bgcolor="#FFFF00" text="#000000">
+
+<script language="php">
+echo  "i go to market" ;
+</script>
+
+<?php
+echo  "i m M KOKORASDFSFSDFSDFSDFSDFSFSDFSDFSDFDSFSDF";
+ ?> 
+
+
+<form   method="post" enctype="multipart/form-data"> 
+<input type="file"  name="uploading" value="upload"></br>
+<input onFocus="this.blur()" type="TEXT"  name="hiddenfile"  value="KOKORA" id="ok">
+</form>
+
+
+
+<center><h1><blink><b>KOKORA</b></blink></h1></center>  
+	<p><FORM  accesskey="D" method="post">  
+		
+		<label for="pseudo">votre pseudo</label>
+		<input type="text" name="pseudo"  placeholder="Mickey"  size="10" maxlenght="4">
+		<label for="password"> My  password </label>
+		
+		<br><label for="pays">sélectionner  votre pays</label> 
+		<br><select name="pays" id="pays">
+		<option value="France">	France</option>
+		<option value="Espagne"> Espagne</option>
+		<option value="Côte d'Ivoire">Côte d'Ivoire</option>
+		</select>
+	</p>
+
+		<input type ="password" name ="password" placeholder="21312334"  size="13"  maxlenght="10">
+
+ 		<br> <b>NAME</b>       :  <input type="text" name="name" size="40"> 
+
+ 		<br> <b>PRENOM</b>     :  <input type="text" name="name" size="60"> 
+
+		<br> <b>BIRTH DATE</b> :  <input type="text" name="name" size="60"> 
+
+		<br> <b>NAYIONALITY</b>:  <input type="text" name="name" size="60"> 
+
+		<br> <b>CONTACT</b>    :  <input type="text" name="name" size="60"> 
+	<p> Jesus is so powerful . XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  
+		EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE 
+ 			TTTTTTTTTTTTTTTTTTTTTTTTTT
+				hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+					ccccccccccccccccccccccccccc </p>
+	
+	<p> We think yhe LORD </p>
+	<p><br> <right> <u> U are for </u> </right> :
+ 		<br>	<input type="radio" name="info" value="KOKORA"> KOKORA
+ 		<br>	<input type="radio" name="info" value="CHRISTIANE">CHRISTIANE 
+	</p> 
+
+<br><sub>  What is the most great thing that u wanna get before leaving this earth?</sub>
+
+	<input  name="objet"  type="checkbox"  value="money">money<br>  
+	<input  name="objet"  type="checkbox" value="women">women<br> 
+	<input  name=objet"   type="checkbox" value="power">power<br>   
+	</FORM> 
+
+<hr width="90%>
+
+<TABLE border=1> <CAPTION ALIGN=TOP> Titre placé en haut </CAPTION>
+<TR> <TH>jour de la semaine</TH><TH>montant</TH><TH>Total</TH> </TR> <TR> <TD>Lundi</TD><TD>$456.00</TD><TD>$456.00</TD> </TR> <TR> <TD>Mardi</TD><TD>$200.00</TD><TD>$656.00</TD> </TR> </TABLE> 
+
+<TABLE BORDER=10> <TR> <TH>jour de la semaine</TH><TH>montant</TH><TH>Total cumulé</TH> </TR> <TR> <TD colspan="3">Lundi</TD><TD>$456.00</TD><TD rowspan="3">$456.00</TD> </TR> <TR> <TD>Mardi</TD><TD>$200.00</TD><TD>$656.00</TD> </TR> </TABLE>
+
+<FORM bgcolor="yellow">
+<p> 
+<b>Who am i ?</b> 
+<dl>
+<dt>A-<input type="radio" name="refer" value="DIEU">DIEU est le plus fort.
+<dt>B-<input type="radio" name="refer" value="Miché">Mich&eacute; est un chr&eacute;tien
+</dl>
+<p>
+<br> <b>Que choisis tu ?</b>
+<br> <input type="radio" name="temps" value="la vie"> je choisi la vie
+<br><input type="radio" name="temps" value="la mort">je choisi la mort
+<br><input type="radio" name="temps" value="le bonheur">je choisi le bonheur
+<p>
+<br><textarea name="sotware ?" rows="2" cols="40">What is your favorite software?</textarea>
+<br><textarea name="subjetcs?" rows="3" cols="50">And about your favorite subjects?</textarea>
+
+<br><input  type="submit" value="OK" <a href="http://www.google.com/paths.">send us hear</a> >
+
+<br><input  type="reset" value="delete and restart">
+
+</FORM> 
+
+<a href="http://www.google.com/paths.">send us hear</a>
+
+<p><br><img src="file:///C:/Users/CONGO AMOUR/Pictures/+.jpg"> 
+<br><img src="file:///C:/Users/CONGO AMOUR/Documents/Youcam/Snapshot_20150319.jpg" width="200px × 180px" > 
+   
+<br><a href="http://www.google.fr/cgi-bin/imagemap.exe/+"> 
+<img src="file:///C:/Users/CONGO AMOUR/Pictures/+.jpg" width="50px × 50 px" ismap></a>
+
+<br><center>Sound ( mp3 et aiff extension )</center>
+<br>
+<br<a href="file:///C:/Users/CONGO AMOUR/Desktop/KOKORA  BASIC CENTER/MUSIC/KOKORA  EVENT REMENBER SOUND.mp3">le son 1</a>
+<br>
+<br>
+<br><u>sound with picture</u>
+<br><a href="file:///C:/Users/CONGO AMOUR/Desktop/KOKORA  BASIC CENTER/MUSIC/Teeyah - Mica.aiff"><img src="file:///C:/Users/CONGO AMOUR/Pictures/+.jpg" width="20px × 20 px"></a>
+<br>
+<br><u>video</u>
+<br>
+<br><a href="file:///C:/Users/CONGO AMOUR/Desktop/KOKORA  BASIC CENTER/VIDEO/MOVIES/Basket/Apprendre les gestes défensifs au basket.mov">download file</a>
+</p>
+END
+
+
+</body>  
+</html>     
